8c80ec173850d7c93c514f6cd8d4e48e1844702b
Jan Pecinovsky <janpecinovsky@gmail.com>
2018-10-11 16:05:43 +0200

    delete test file

--- test.py
+++
@@ -1,5 +0,0 @@
-from pyEnFace.EnphaseInterface import PandasEnphaseInterface
-
-client = PandasEnphaseInterface(userId='4d7a45774e6a41320a', api_key='5965b2a3a2022ea5f2cfd7f2184bcabb')
-
-client.energy_lifetime(system_id=67)
